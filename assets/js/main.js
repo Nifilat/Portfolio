37d25d4220fcd19b60d8886c86a7492fad596499
Oluwanifemi <latunde.nifemi2016@gmail.com>
2023-05-29 06:45:45 +0100

    Contact Form with EmailJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,41 @@ const blurHeader = () => {
 window.addEventListener("scroll", blurHeader);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
 
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // serviceID - templateID - #form -publicKey
+  emailjs
+    .sendForm(
+      "service_q5amvka",
+      "template_p9cnh8c",
+      "#contact-form",
+      "TF-tECVX1YmMHFdG8"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
